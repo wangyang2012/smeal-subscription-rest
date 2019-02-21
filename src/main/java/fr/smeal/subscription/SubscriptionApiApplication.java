3f3cfee247fc9b1e87c451b232e7ba635b0f28cd
yang <wangyang1712@gmail.com>
2019-02-21 02:56:34 +0100

    add import

--- src/main/java/fr/smeal/subscription/SubscriptionApiApplication.java
+++ src/main/java/fr/smeal/subscription/SubscriptionApiApplication.java
@@ -2,6 +2,7 @@ package fr.smeal.subscription;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class SubscriptionApiApplication extends SpringBootServletInitializer {
